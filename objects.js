77541084cf4d94335f9a378ce7a07b919fc9cc3f
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2025-01-04 07:40:49 -0400

    I need to practice more all of those consepts

--- objects.js
+++ objects.js
@@ -1,35 +1,24 @@
-// Reto 3
+// Reto 1.3: Objetos y Destructuring
 // Define the user object
+
+// 1. Crea un objeto que represente un usuario con propiedades como: nombre, edad, pais, hobbies (array).
 const user = {
   name: "Pablo",
   age: 24,
   country: "Mexico",
   hobbies: ["Gaming", "Reading", "Coding"],
+  // 2. Añade un método dentro del objeto que retorne un saludo usando el nombre y pais.
   greating: function () {
     console.log("Hello, my name is " + this.name + " and I'm from " + this.country);
   }
 };
 
-// Accessing properties using dot notation
-console.log("User details (dot notation):");
-console.log("Name:", user.name);
-console.log("Age:", user.age);
-console.log("Hobbies", user.hobbies);
-
-// Accessing properties using bracket notation
-console.log("User details (bracket notation):");
-console.log("Name:", user["name"]);
-console.log("Country:", user["country"]);
-console.log("Hobbies:", user["hobbies"]);
-
-// Example of undefined property using bracket notation
-const property = "isStudent"; // This property doesn't exist in the object
-console.log("Is student (undefined):", user[property]);
-
+// 3. Desde fuera del objeto, llama ese método e imprime el resultado.
 // Calling the method
 console.log("\nCalling the 'greating' method:");
 user.greating();
 
+// 4. Utiliza destructuring para acceder a edad y hobbies en una sola línea.
 // Using destructuring to extract specific properties
 const { age, hobbies } = user;
 console.log("\nDestructured properties:");
