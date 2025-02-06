de44114526441234bc6c36937baf430bddc6c57a
Andy-Fernandez <58851171+Andy-Fernandez@users.noreply.github.com>
2025-02-06 17:19:35 -0400

    Ecercise 2 done, we need to take a vew

--- 11. Currency-Convert/converter.mjs
+++ 11. Currency-Convert/converter.mjs
@@ -18,24 +18,25 @@ export async function convertSpecific(amount, fromCurrency, toCurrency) {
 export async function multiCurrencyConverter(fromCurrency) {
   try {
     const data = await fetchCurrency(fromCurrency);
+    const conversion_rates = data.conversion_rates;
     const usd = conversion_rates['USD'];
-    // const jpy = conversion_rates['JPY'];
-    // const gbp = conversion_rates['GBP'];
-    // const eur = conversion_rates['EUR'];
+    const jpy = conversion_rates['JPY'];
+    const gbp = conversion_rates['GBP'];
+    const eur = conversion_rates['EUR'];
     const from = conversion_rates[fromCurrency];
 
-    // console.log(convert(100, from, usd));
-    // console.log(convert(100, from, jpy));
-    // console.log(convert(100, from, gbp));
-    // console.log(convert(100, from, eur));
-    console.log(data);
+    console.log(convert(100, usd));
+    console.log(convert(100, eur));
+    console.log(convert(100, jpy));
+    console.log(convert(100, gbp));
+    // console.log(data);
     
   } catch (error) {
     console.error("Error:", error.message);
   }
 }
 
-const convert = (amount ,fromRate, toRate) => (amount * (fromRate/toRate)).toFixed(2);
+export const convert = (amount, toRate) => (amount * (toRate)).toFixed(2);
 
 multiCurrencyConverter('USD')
   .then((message) => console.log(message))
--- 11. Currency-Convert/domHandler.mjs
+++ 11. Currency-Convert/domHandler.mjs
@@ -1,11 +1,21 @@
-import { convertSpecific } from "./converter.mjs";
+import { convertSpecific, convert } from "./converter.mjs";
+import { fetchCurrency } from "./api.mjs";
+
 
 export function setupEventListeners() {
   const amount = document.getElementById("amount");
   const fromCurrency = document.getElementById("fromCurrency");
   const toCurrency = document.getElementById("toCurrency");
   const convertBtn = document.getElementById("convertBtn");
+  const convertBtnMulti = document.getElementById("convertBtnMulti");
   const resultDiv = document.getElementById("result");
+  
+  const fromCurrencyMulti = document.getElementById("fromCurrencyMulti");
+  const resultUsd = document.getElementById("resultUsd")
+  const resultEur = document.getElementById("resultEur")
+  const resultJpy = document.getElementById("resultJpy")
+  const resultGbp = document.getElementById("resultGbp")
+
 
   convertBtn.addEventListener("click", async () => {
     try {
@@ -24,4 +34,34 @@ export function setupEventListeners() {
       console.error("Error:", error.message);
     }
   });
+
+  convertBtnMulti.addEventListener("click", async () => {
+    try {
+      const from = fromCurrencyMulti.value.trim();
+      const data = await fetchCurrency(from);
+      const conversion_rates = data.conversion_rates;
+      const usd = conversion_rates['USD'];
+      const jpy = conversion_rates['JPY'];
+      const gbp = conversion_rates['GBP'];
+      const eur = conversion_rates['EUR'];
+  
+      resultUsd.innerText = "USD: " + convert(100, usd);
+      resultEur.innerText = "EUR: " + convert(100, eur);
+      resultJpy.innerText = "JPY: " + convert(100, jpy);
+      resultGbp.innerText = "GBP: " + convert(100, gbp);
+
+      // console.log(data);
+      console.log(
+        convert(100, usd),
+        convert(100, eur),
+        convert(100, jpy),
+        convert(100, gbp),
+      );
+      
+      
+    } catch (error) {
+      console.error("Error:", error.message);
+    }
+})
+
 }
